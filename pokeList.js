3ea0f95e3a6ed432eefe3a5d8eeaeddf2823f416
Academy-of-Code <77977795+Academy-of-Code@users.noreply.github.com>
2021-03-04 14:15:57 -0500

    Create pokeList.js

---
+++ pokeList.js
@@ -0,0 +1,45 @@
+var kyoger = {
+name: "Kyoger",
+maxHealth: 100,
+health: 100, 
+type: "Water", 
+weakness1: "Grass", 
+weakness2: "Electric", 
+level: 60, 
+indexNum: 382,
+shiny: false,
+primal: false,
+sprite: "https://veekun.com/dex/media/pokemon/main-sprites/ultra-sun-ultra-moon/382.png",
+
+primalSprite: "https://veekun.com/dex/media/pokemon/main-sprites/ultra-sun-ultra-moon/382-primal.png",
+
+shinySprite: "https://veekun.com/dex/media/pokemon/main-sprites/ultra-sun-ultra-moon/shiny/382.png",
+
+shinyPrimalSprite: "https://veekun.com/dex/media/pokemon/main-sprites/ultra-sun-ultra-moon/shiny/382-primal.png",
+
+move1: {
+moveName: "Scary Face",
+pp: 10,
+power: null,
+accuracy: 100,
+},
+move2: {
+moveName: "Origin Pulse",
+pp: 10,
+power: 110,
+accuracy: 85,
+},
+move3: {
+moveName: "Ice Beam",
+pp: 10,
+power: 90,
+accuracy: 100,
+},
+move4: {
+moveName: "Aqua Ring",
+pp: 20,
+power: null,
+accuracy: null,
+},
+itemHeld: null,
+}
