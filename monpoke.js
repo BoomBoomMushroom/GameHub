7d1411316d767e9bfac550b76ee2228fa719de08
Academy-of-Code <77977795+Academy-of-Code@users.noreply.github.com>
2021-03-09 08:15:28 -0500

    Update monpoke.js

--- monpoke.js
+++ monpoke.js
@@ -5,12 +5,22 @@ var routeNumber = -1;
 var possibleEntitys = [kyoger, groudon, raquaza]
 var entityPoke = null;
 
+var shinyChance = 8192;
+var megaChance = 5000;
+
 var poke1 = null
 var poke2 = null
 var poke3 = null
 var poke4 = null
 var poke5 = null
 var poke6 = null
+
+poke1 = null
+poke2 = null
+poke3 = null
+poke4 = null
+poke5 = null
+poke6 = null
 myStorage = window.localStorage;
 
 var name = "UNDEFINED"
@@ -31,12 +41,12 @@ function play() {
 function encounterEntity() {
 	autoSave()
   function shinyCalculate(pokemon) {
-    if (randomNum(1, 8192/*8192*/) === 1) {
+    if (randomNum(1, shinyChance/*8192*/) === 1) {
       pokemon.shiny = true
     } else {
       pokemon.shiny = false
     }
-    if(randomNum(1, 5000 /*5000*/) === 1){
+    if(randomNum(1, megaChance /*5000*/) === 1){
     	pokemon.mega = true
     }
     else{
@@ -152,9 +162,13 @@ function clearAllData(){
 
 function catchMon(){
 	var d = new Date();
-  var e = d.getMonth()+"/"+d.getDate()+"/"+d.getFullYear()
+  var e = d.getMonth()+1+"/"+d.getDate()+"/"+d.getFullYear()
   
   document.getElementById("name").innerHTML = "Caught by "+name+", on "+e
+  
+  document.getElementById("monGot").src = document.getElementById("enemyEntity").src
+  
+  battleGrounds.style.display = 'none'
 }
 function possibleEntitysUpdate() {
   if (routeNumber === -1) {
@@ -170,4 +184,131 @@ function firstStartLoad() {
   battleGrounds.style.display = 'none'
   possibleEntitysUpdate()
   autoLoad()
+}
+function sha256(ascii) {
+	function rightRotate(value, amount) {
+		return (value>>>amount) | (value<<(32 - amount));
+	};
+	
+	var mathPow = Math.pow;
+	var maxWord = mathPow(2, 32);
+	var lengthProperty = 'length'
+	var i, j; // Used as a counter across the whole file
+	var result = ''
+
+	var words = [];
+	var asciiBitLength = ascii[lengthProperty]*8;
+	
+	//* caching results is optional - remove/add slash from front of this line to toggle
+	// Initial hash value: first 32 bits of the fractional parts of the square roots of the first 8 primes
+	// (we actually calculate the first 64, but extra values are just ignored)
+	var hash = sha256.h = sha256.h || [];
+	// Round constants: first 32 bits of the fractional parts of the cube roots of the first 64 primes
+	var k = sha256.k = sha256.k || [];
+	var primeCounter = k[lengthProperty];
+	/*/
+	var hash = [], k = [];
+	var primeCounter = 0;
+	//*/
+
+	var isComposite = {};
+	for (var candidate = 2; primeCounter < 64; candidate++) {
+		if (!isComposite[candidate]) {
+			for (i = 0; i < 313; i += candidate) {
+				isComposite[i] = candidate;
+			}
+			hash[primeCounter] = (mathPow(candidate, .5)*maxWord)|0;
+			k[primeCounter++] = (mathPow(candidate, 1/3)*maxWord)|0;
+		}
+	}
+	
+	ascii += '\x80' // Append Ƈ' bit (plus zero padding)
+	while (ascii[lengthProperty]%64 - 56) ascii += '\x00' // More zero padding
+	for (i = 0; i < ascii[lengthProperty]; i++) {
+		j = ascii.charCodeAt(i);
+		if (j>>8) return; // ASCII check: only accept characters in range 0-255
+		words[i>>2] |= j << ((3 - i)%4)*8;
+	}
+	words[words[lengthProperty]] = ((asciiBitLength/maxWord)|0);
+	words[words[lengthProperty]] = (asciiBitLength)
+	
+	// process each chunk
+	for (j = 0; j < words[lengthProperty];) {
+		var w = words.slice(j, j += 16); // The message is expanded into 64 words as part of the iteration
+		var oldHash = hash;
+		// This is now the undefinedworking hash", often labelled as variables a...g
+		// (we have to truncate as well, otherwise extra entries at the end accumulate
+		hash = hash.slice(0, 8);
+		
+		for (i = 0; i < 64; i++) {
+			var i2 = i + j;
+			// Expand the message into 64 words
+			// Used below if 
+			var w15 = w[i - 15], w2 = w[i - 2];
+
+			// Iterate
+			var a = hash[0], e = hash[4];
+			var temp1 = hash[7]
+				+ (rightRotate(e, 6) ^ rightRotate(e, 11) ^ rightRotate(e, 25)) // S1
+				+ ((e&hash[5])^((~e)&hash[6])) // ch
+				+ k[i]
+				// Expand the message schedule if needed
+				+ (w[i] = (i < 16) ? w[i] : (
+						w[i - 16]
+						+ (rightRotate(w15, 7) ^ rightRotate(w15, 18) ^ (w15>>>3)) // s0
+						+ w[i - 7]
+						+ (rightRotate(w2, 17) ^ rightRotate(w2, 19) ^ (w2>>>10)) // s1
+					)|0
+				);
+			// This is only used once, so *could* be moved below, but it only saves 4 bytes and makes things unreadble
+			var temp2 = (rightRotate(a, 2) ^ rightRotate(a, 13) ^ rightRotate(a, 22)) // S0
+				+ ((a&hash[1])^(a&hash[2])^(hash[1]&hash[2])); // maj
+			
+			hash = [(temp1 + temp2)|0].concat(hash); // We don't bother trimming off the extra ones, they're harmless as long as we're truncating when we do the slice()
+			hash[4] = (hash[4] + temp1)|0;
+		}
+		
+		for (i = 0; i < 8; i++) {
+			hash[i] = (hash[i] + oldHash[i])|0;
+		}
+	}
+	
+	for (i = 0; i < 8; i++) {
+		for (j = 3; j + 1; j--) {
+			var b = (hash[i]>>(j*8))&255;
+			result += ((b < 16) ? 0 : '') + b.toString(16);
+		}
+	}
+	return result;
+};
+function adminLogin(){
+	var inputUsername = window.prompt("Enter username: ")
+  var inputPassword = window.prompt("Enter password: ")
+  
+  var hashedUsername = sha256(inputUsername);
+  var hashedPassword = sha256(inputPassword);
+  
+  var user = 'e98ae3a385900e101b403c3b87a8f291fdf5d17c1ec55612154ac2b25da228bd';
+  var pass = 'f61374852ca5c60c54d4a501c4bb051bb3f83aafe42aa58ff1f325fa3ad71e66';
+  
+  var user2 = '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918'
+  var pass2 = '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'
+  
+  if (hashedUsername === user && hashedPassword === pass || hashedUsername === user2 && hashedPassword === pass2)
+  {
+  	openPanel()
+  }
+}
+
+function updateShinyValue(){
+	var ssv = document.getElementById("setShinyValue")
+	shinyChance = parseInt(ssv.value)
+}
+function openPanel(){
+	var panel = document.getElementById("adminPanel")
+  panel.style.display = 'block'
+}
+function closePanel(){
+	var panel = document.getElementById("adminPanel")
+  panel.style.display = 'none'
 }
