8d45cbcf358f4c90c0496bbf0031556c435d98a9
BoomBoomMushroom <77977795+BoomBoomMushroom@users.noreply.github.com>
2021-09-01 16:21:50 -0400

    Create darkmode_loader.js

---
+++ js_modules/darkmode_loader.js
@@ -0,0 +1,45 @@
+function loader(){
+	function setCookie(cname, cvalue, exdays){
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname+"="+cvalue+";"+expires+";path=/";
+  }
+  function getCookie(cname){
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i=0; i<ca.length;i++){
+      let c = ca[i];
+      while (c.charAt(0)==' '){
+        c = c.substring(1);
+      }
+      if(c.indexOf(name)==0){
+        return c.substring(name.length,c.length);
+      }
+    }
+    return "";
+  }
+  function getDarkmode(cookieName){
+  	if(getCookie(cookieName)==""){
+    	return false
+    }
+    else{
+    	return true
+    }
+  }
+  darkmode = getDarkmode("theme")
+  
+  if(darkmode==true){
+    var darkmodeColor = "#2C2F33"
+    paragraphs = document.getElementsByTagName("p")
+    document.body.style.background = darkmodeColor
+    for(var x=0;x<paragraphs.length;x++){
+      para = paragraphs[x]
+      para.style.color = "#ffffff"
+      // para.classList.add("darkmode_text")
+    }
+  }
+}
+
+document.onload = loader()
