989e0ffb350766f15510fc3976bf66d6ca4cd019
BoomBoomMushroom <77977795+BoomBoomMushroom@users.noreply.github.com>
2023-02-18 13:21:57 -0500

    did some stuff to gameshub and cleaned up the scriprs

---
+++ js_modules/bigRedButton.js
@@ -0,0 +1,22 @@
+function randomInt(min,max){
+    return Math.floor(Math.random() * (max-min+1))+min
+}
+
+document.getElementById("bigRedButton").addEventListener("click",(e)=>{
+var button = document.getElementById("bigRedButton")
+button.style.top = randomInt(0,window.innerHeight-button.offsetHeight)+"px"
+button.style.left = randomInt(0,window.innerWidth-button.offsetWidth)+"px"
+var cl = parseInt( getCookie("clicks") )
+if(isNaN(cl)){
+    cl = 0
+}
+setCookie("clicks",(cl+1).toString(),999)
+button.innerText = (cl+1).toString()
+})
+
+document.getElementById("bigRedButton").addEventListener("click", function(e){
+    fetch("https://gameshub-api.academyofcode1.repl.co/registerButtonClicks?token="+getCookie("accToken")+"&amount="+getCookie("clicks"))
+    .then(d=>{
+    console.log(d)
+    })
+})
---
+++ js_modules/loadAccount.js
@@ -0,0 +1,33 @@
+// Get Account
+fetch('https://Gameshub-API.academyofcode1.repl.co/tokeninfo?token='+getCookie("accToken"),{})
+.then(response => response.text())
+.then(data => {
+    d = JSON.parse(data)
+    console.log(JSON.parse(data))
+    try{
+        if (d["GameshubData"]["RedButtonClicks"] >= 666){
+            backgrounds.push( "https://raw.githubusercontent.com/BoomBoomMushroom/GameHub/main/GamehubApi/images/666_Clicks.png" )
+            if(Math.random() <= 1 / backgrounds.length){
+            document.body.style.backgroundImage="url('"+"https://raw.githubusercontent.com/BoomBoomMushroom/GameHub/main/GamehubApi/images/666_Clicks.png"+"')"
+            }
+        }
+  
+        if(JSON.parse(data)["GameshubData"]["RedButtonClicks"]){
+            setCookie("clicks",JSON.parse(data).GameshubData.RedButtonClicks.toString(),999)
+        }
+    }
+    catch(e){console.log(e)}
+    
+    if(JSON.parse(data)=="COULDNT_FIND_TOKEN"){
+        document.getElementById("currentAcc").href = "https://gameshub.dev/gamehubapi/signup"
+        document.getElementById("currentAcc").innerText = "You are currently not signed in"
+        document.getElementById("logout").style.display = "none"
+    }
+    else{
+        document.getElementById("currentAcc").href = "https://gameshub.dev/gamehubapi/viewacc?q=" + JSON.parse(data).UUID
+        //document.getElementById("currentAcc").href = "https://gameshub.dev/gamehubapi/accSettings"
+        document.getElementById("currentAcc").innerText = "Currently signed in as " + JSON.parse(data).Username
+        document.getElementById("tr1").style.display = "none"
+        document.getElementById("tr2").style.display = "none"
+    }
+})
