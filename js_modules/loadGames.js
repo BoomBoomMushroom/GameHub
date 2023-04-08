af46539c6a7461a6c6c9ae073967dd029425faa9
BoomBoomMushroom <77977795+BoomBoomMushroom@users.noreply.github.com>
2023-04-08 17:04:02 -0400

    Update loadGames.js

--- js_modules/loadGames.js
+++ js_modules/loadGames.js
@@ -44,6 +44,7 @@ var economy = [
     {name:"Blackjack", url: "./games/blackjack"},
     {name:"Slot Machines", url: "./games/slotMachines"},
     {name:"Rooulette", url: "./games/rooulette"},
+    {name:"", url: ""}, // Spacing, to make it look less cluttered
     {name:"Coder Job (Earn Money)", url: "./job/coder_job"},
 ]
 
